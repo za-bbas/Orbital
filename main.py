50f7d7c7ea03a1694a34a5209a6fe0725331f4c9
Zaid Abbas <zaid.sarmad2006@gmail.com>
2025-07-31 10:58:33 -0700

    utils functions moved

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from planet import *
 from params import *
+from utils import *
 from satellite import Satellite
 import numpy as np
 import numpy.linalg as la
@@ -11,33 +12,6 @@ import matplotlib.pyplot as plt
 # import ppigrf
 import time # for benchmarking
 
-# the whole euler angles thing kind makes me blegh but its good enough for now
-def eulerToQuat(angles):
-    phi = angles[0]
-    theta = angles[1]
-    psi = angles[2]
-
-    cph = np.cos(phi / 2)
-    cth = np.cos(theta / 2)
-    cps = np.cos(psi / 2)
-    sph = np.sin(phi / 2)
-    sth = np.sin(theta / 2)
-    sps = np.sin(psi / 2)
-
-    q0 = cph * cth * cps + sph * sth * sps
-    q1 = sph * cth * cps - cph * sth * sps
-    q2 = cph * sth * cps + sph * cth * sps
-    q3 = cph * cth * sps - sph * sth * cps
-    
-    return np.array([q0, q1, q2, q3])
-
-def quatToEuler(quat):
-    q0, q1, q2, q3 = quat
-    phi = np.arctan2(2*(q0*q1 + q2*q3), 1 - 2*(q1**2 + q2**2))
-    theta = np.arcsin(2*(q0*q2 - q1*q3))
-    psi = np.arctan2(2*(q0*q3 + q1*q2), 1 - 2*(q2**2 + q3**2))
-
-    return np.array([phi, theta, psi])
 
 # Simulation of a low earth satellite
 print('Simulation started.')
